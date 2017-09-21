af76bfccafe0bad68be8dbc1edf4f38ac6151463
Jay Dobies <jason.dobies@redhat.com>
2017-09-21 14:34:02 -0400

    Finished base functionality (DB untested)

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
   <style>
         .table-striped>tbody>tr:nth-child(odd)>td,
@@ -29,6 +31,7 @@
   <h1 id="heading" style="text-align: center">Red Hat OpenShift - PHP & Database Demo</h1>
 
 <?php
+error_reporting(E_ERROR);
 
 $host = getenv("MYSQL_SERVICE_HOST");
 $port = getenv("MYSQL_SERVICE_PORT");
@@ -36,19 +39,51 @@ $database = getenv("MYSQL_SERVICE_DATABASE");
 $username = getenv("MYSQL_SERVICE_USERNAME");
 $password = getenv("MYSQL_SERVICE_PASSWORD");
 
-?>
+$conn = mysqli_connect($host, $username, $password, $database, $port);
 
-  <div class="details">
-    <table class="table table-bordered table-striped table-responsive table-hover">
-
-<?php
-error_reporting(E_ERROR);
+$page_count = 'Unknown';
 
 function write_key_value($key, $value) {
   echo '<tr><td class="key">' . $key . '</td>';
   echo '<td><p id="ip">' . $value . '</p></td></tr>';
 }
 
+?>
+
+<?php
+if ($conn) {
+
+  // Initialize the DB if necessary
+  $sql = "CREATE TABLE visitors (
+          id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+          containerip VARCHAR(15) NOT NULL,
+          visitstamp VARCHAR(30) NOT NULL)";
+  mysqli_query($conn, $sql);
+
+  // Add entry for this request
+  $containerip = $_SERVER['SERVER_ADDR'];
+  $visitstamp = date("D M j G:i:s T Y");
+  $sql = "INSERT INTO visitors (containerip, visitstamp)
+          VALUES ('$containerip', '$visitstamp')";
+  mysqli_query($conn, $sql);
+
+  // Update the count
+  $sql = 'SELECT COUNT(*) FROM visitors';
+  $page_count = mysqli_query($conn, $sql);
+}
+
+?>
+
+  <div class="details">
+
+    <table class="table table-bordered table-striped table-responsive table-hover">
+<?php
+  write_key_value('Page Hits', $page_count)
+?>
+    </table>
+
+    <table class="table table-bordered table-striped table-responsive table-hover">
+<?php
 // Server Details
 $env_text = getenv("TEXT");
 $uptime = shell_exec('uptime -p');
@@ -57,47 +92,16 @@ $cores = trim(shell_exec("cat /proc/cpuinfo | grep processor | wc -l"));
 
 write_key_value('Server IP', $_SERVER['SERVER_ADDR']);
 write_key_value('Database Name', ($database != '' ? $database : 'Unknown'));
-write_key_value('Environment Variable (TEXT)', ($env_text != '' ? $env_text : 'Unknown'));
+write_key_value('Environment Variable (TEXT)', $env_text);
 write_key_value('Server Uptime', $uptime);
 write_key_value('CPU', $cpu);
 write_key_value('Number of Cores', $cores);
 ?>
     </table>
-  </div>
 
 <?php
-$conn = mysqli_connect($host, $username, $password, $database, $port);
 
 if ($conn) {
-
-  echo "Database is available <br/><br/>";
-  $sql = "CREATE TABLE visitors (
-          id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-          containerip VARCHAR(15) NOT NULL,
-          visitstamp VARCHAR(30) NOT NULL)";
-  if (mysqli_query($conn, $sql)) {
-    echo "Database initialized <br/>";
-  } else {
-    echo "Database initialization skipped: ";
-    echo mysqli_error($conn) . "<br/>";
-  }
-
-  echo "<br/>";
-
-  $containerip = $_SERVER['SERVER_ADDR'];
-  $visitstamp = date("D M j G:i:s T Y");
-  $sql = "INSERT INTO visitors (containerip, visitstamp)
-          VALUES ('$containerip', '$visitstamp')";
-
-  if (mysqli_query($conn, $sql)) {
-    echo "New visitor logged <br/>";
-  } else {
-    echo "Error: " . $sql . " - " . mysqli_error($conn) . "<br/>";
-  }
-
-  echo "<br/>";
-
-  echo "<h3> Visitor Log </h3>";
   $sql = "SELECT id, containerip, visitstamp FROM visitors ORDER BY id DESC LIMIT 20";
   $result = mysqli_query($conn, $sql);
   if (mysqli_num_rows($result) > 0) {
@@ -114,8 +118,7 @@ if ($conn) {
 }
 ?>
 
-  <br/><br/>
-  <img src="powered-transparent-black.png" alt="Powered by OpenShift"/>
+  </div>
 
 </body>
 
